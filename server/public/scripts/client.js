cfdf66c9fad6661067e761373b1ed4f4108297d6
Kalia Vang <kaliavang511@gmail.com>
2024-05-18 14:42:44 -0500

    get and post work

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,1 +1,65 @@
-console.log('JS is sourced!');
+getToDos();
+
+function getToDos() {
+    axios.get('/todos').then((response) => {
+        appendToDosToDom(response.data);
+    });
+}
+
+
+function appendToDosToDom(toDoList) {
+    let toDosBody = document.querySelector("#outPut")
+    toDosBody.innerHTML = '';
+    for (let toDos of toDoList) {
+        toDosBody.innerHTML += `
+       
+                <li data-testid="toDoItem" class="completed">
+                ${toDos.text}
+                </li>
+
+                <li>
+                <button onClick="deleteToDos"(${toDos.id})">
+                delete
+                </button>
+               </li>
+
+      
+      
+                
+        `
+    }
+}
+
+
+function postList(event){
+    event.preventDefault();
+    let payloadObject = {
+        text: document.querySelector('#todosinput').value
+    }
+ console.log(payloadObject)
+
+    axios.post('/todos', payloadObject).then((response) => {
+        getToDos();
+    }).catch((error) => {
+        alert('Something went wrong');
+    });
+  
+}
+
+
+
+
+function deleteToDos(toDosId) {
+    axios({
+        method: "DELETE",
+        url: `/todos/${toDosId}`
+    })
+    .then((response) => {
+        getToDos();
+    }).catch((error) => {
+        console.log('Error', error);
+        alert('Something went wrong');
+    });
+}
+
+
--- server/routes/todos.router.js
+++ server/routes/todos.router.js
@@ -1,6 +1,61 @@
 const router = require('express').Router();
 const pool = require('../modules/pool');
 
+router.get('/', (req, res) => {
+    let queryText = `SELECT * FROM "todos";`
+    pool.query(queryText)
+        .then((result) => {
+            console.log("Result from DB:", result.rows)
+            res.send(result.rows)
+        })
+        .catch((err) => {
+            console.log(`Error making query.. '${queryText}'`, err)
+            res.sendStatus(500)
+        })
+});
+
+router.post('/', (req, res) => {
+    console.log('req.body', req.body);
+
+    let toDos = req.body
+    
+    let toDosArray = [toDos.text]
+    let queryText = `
+        INSERT INTO "todos" ("text")
+        VALUES ($1);
+    `
+    pool.query(queryText, toDosArray)
+        .then((result) => {
+            res.sendStatus(201)
+        })
+        .catch((err) => {
+            console.log(`Error making query.. '${queryText}'`, err)
+            res.sendStatus(500)
+        })
+});
+
+
+
+router.delete('/:id', (req, res) => {
+    let queryText = `
+        DELETE FROM "todos"  WHERE "id" = $1;
+    `
+
+    let reqId = [req.params.id]
+
+    pool.query(queryText, reqId)
+        .then((result) => {
+            console.log("Successfully Deleted...")
+            res.sendStatus(200)
+        })
+        .catch((err) => {
+            console.log(`Error making query.. '${queryText}'`, err)
+            res.sendStatus(500)
+        })
+});
+
+
+
 
 
 
